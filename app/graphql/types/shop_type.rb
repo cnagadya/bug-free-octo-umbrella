69272c9f021dfcff97bd27840bd87958c484b763
cnagadya <cnagadya@github.com>
2022-07-12 17:48:07 +0000

    Use trilogy

--- app/graphql/types/shop_type.rb
+++ app/graphql/types/shop_type.rb
@@ -2,7 +2,7 @@ module Types
   class ShopType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: true
-    field :items, ItemType.connection_type, null: true, max_page_size: 25
+    field :items, ItemType.connection_type, null: true, max_page_size: 5
 
     # def items
     #   ActiveRecord::Base.connected_to(role: :reading) do
